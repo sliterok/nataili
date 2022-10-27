781623a2274c51e3dd2d93470876257942bc2025
blueturtleai <113246030+blueturtleai@users.noreply.github.com>
2022-10-27 15:22:08 +0200

    Update test_inpainting.py

--- test_inpainting.py
+++ test_inpainting.py
@@ -4,7 +4,7 @@ from PIL import Image
 original = Image.open("./inpaint_original.png")
 mask = Image.open("./inpaint_mask.png")
 
-generator = inpainting("output_dir", "cuda")
-images = generator.generate("a mecha robot sitting on a bench", original, mask)
-image = images[0]
+generator = inpainting("cuda", "output_dir")
+generator.generate("a mecha robot sitting on a bench", original, mask)
+image = generator.images[0]["image"]
 image.save("robot_sitting_on_a_bench.png", format="PNG")
